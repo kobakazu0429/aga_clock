f55c65ef22666c19d8bacd515f9566ace746cd10
kobakakzu0429 <kobakazu10@gmail.com>
2019-01-17 23:52:03 +0900

    add alarm

--- aga_clock.ino
+++ aga_clock.ino
@@ -5,22 +5,29 @@ SevenSegmentController SevenSegmentController;
 
 void setup() {
   struct dateTime dt = {0, 30, 12, 1, 1, 19, 2};
-  char s[20];
+  struct alarmTime at = {31, 12, 1, 2};
 
   RTC8564.begin(&dt);
+  RTC8564.setAlarm(RTC8564_AE_MINUTE, &at, 0);
+
   String currentClock = "0000";
 }
 
 void loop() {
   struct dateTime dt;
-  char s[20];
+  char RTCTime[20];
 
   if (RTC8564.getDateTime(&dt) == 0) {
-    sprintf(s, "%2d%2d", dt.hour, dt.minute);
+    sprintf(RTCTime, "%2d%2d", dt.hour, dt.minute);
   } else {
     RTC8564.begin();
   }
 
-  String currentClock = s;
+  if (RTC8564.getAlarmFlag()) {
+    // processing ...
+    RTC8564.clearAlarmFlag();
+  }
+
+  String currentClock = RTCTime;
   SevenSegmentController.displayNumbers(currentClock);
 }
