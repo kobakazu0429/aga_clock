f84077150fea23b2b8586a5a3a65f0ea20baa3ca
kobakakzu0429 <kobakazu10@gmail.com>
2019-01-17 23:04:52 +0900

    use rtc

--- aga_clock.ino
+++ aga_clock.ino
@@ -1,11 +1,26 @@
 #include "SevenSegmentController.h"
+#include "RTC8564.h"
 
 SevenSegmentController SevenSegmentController;
 
 void setup() {
+  struct dateTime dt = {0, 30, 12, 1, 1, 19, 2};
+  char s[20];
+
+  RTC8564.begin(&dt);
+  String currentClock = "0000";
 }
 
 void loop() {
-  String currentClock = "0429";
+  struct dateTime dt;
+  char s[20];
+
+  if (RTC8564.getDateTime(&dt) == 0) {
+    sprintf(s, "%2d%2d", dt.hour, dt.minute);
+  } else {
+    RTC8564.begin();
+  }
+
+  String currentClock = s;
   SevenSegmentController.displayNumbers(currentClock);
 }
