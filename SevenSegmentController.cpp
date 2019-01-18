a7456bed20a72cb8e613ad90f8a8acbb6f505e4a
kobakakzu0429 <kobakazu10@gmail.com>
2019-01-18 11:36:04 +0900

    fix

--- SevenSegmentController.cpp
+++ SevenSegmentController.cpp
@@ -2,22 +2,6 @@
 #include "SevenSegmentController.h"
 
 SevenSegmentController::SevenSegmentController() {
-  CATHODE_PINS[0] = 9;
-  CATHODE_PINS[1] = 10;
-  CATHODE_PINS[2] = 11;
-  CATHODE_PINS[3] = 12;
-
-  PATTERNS[0] = 0b11111100;
-  PATTERNS[1] = 0b01100000;
-  PATTERNS[2] = 0b11011010;
-  PATTERNS[3] = 0b11110010;
-  PATTERNS[4] = 0b01100110;
-  PATTERNS[5] = 0b10110110;
-  PATTERNS[6] = 0b10111110;
-  PATTERNS[7] = 0b11100100;
-  PATTERNS[8] = 0b11111110;
-  PATTERNS[9] = 0b11110110;
-
   pinMode(SRCLK, OUTPUT);
   pinMode(RCLK, OUTPUT);
   pinMode(SER, OUTPUT);
--- SevenSegmentController.h
+++ SevenSegmentController.h
@@ -14,10 +14,21 @@ private:
   static const int RCLK = 6;
   static const int SER = 7;
 
-  int CATHODE_PINS[4];
+  int CATHODE_PINS[4] = {9, 10, 11, 12};
   static const int CATHODE_PINS_LENGTH = 4;
 
-  uint8_t PATTERNS[10];
+  uint8_t PATTERNS[10] = {
+    0b11111100,
+    0b01100000,
+    0b11011010,
+    0b11110010,
+    0b01100110,
+    0b10110110,
+    0b10111110,
+    0b11100100,
+    0b11111110,
+    0b11110110
+  };
 };
 
 #endif
