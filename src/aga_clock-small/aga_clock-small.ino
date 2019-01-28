62f49c2a887e26bd5df8e06fb4017c0113d159ab
kobakakzu0429 <kobakazu10@gmail.com>
2019-01-28 19:10:40 +0900

    add

--- src/aga_clock-small/aga_clock-small.ino
+++ src/aga_clock-small/aga_clock-small.ino
@@ -1,25 +1,21 @@
-#include "DFPlayer_Mini_Mp3.h"
 #include "RTC8564.h"
 #include "SevenSegmentController.h"
-#include <SoftwareSerial.h>
 
-int model = 3;
-boolean isPlaying = false;
+// 阿賀小学校         :  0
+// 原小学校           :  1
+// 市民センター       :  2
+// まちづくりセンター :  3
+// デバッグ用         : 99
+
+#define MODEL 99
 
 SevenSegmentController SevenSegmentController;
 
 void setup() {
   pinMode(13, INPUT);
 
-  struct dateTime dt = {0, 0, 13, 1, 1, 19, 2};
-  struct alarmTime at = {5, 13, 1, 2};
-
+  struct dateTime dt = {0, 0, 0, 1, 1, 19, 2};
   RTC8564.begin(&dt);
-  RTC8564.setAlarm((RTC8564_AE_MINUTE | RTC8564_AE_HOUR), &at, 0);
-
-  Serial.begin(9600);
-  mp3_set_serial(Serial);
-  mp3_set_volume(5);
 }
 
 void addMinutes() {
@@ -48,15 +44,15 @@ void addMinutes() {
                    datetime.weekday};
   }
 
-    if (datetime.hour == 23 && datetime.minute == 59) {
-      newDatetime = {0,
-                     0,
-                     0,
-                     datetime.day,
-                     datetime.month,
-                     datetime.year,
-                     datetime.weekday};
-    }
+  if (datetime.hour == 23 && datetime.minute == 59) {
+    newDatetime = {0,
+                   0,
+                   0,
+                   datetime.day,
+                   datetime.month,
+                   datetime.year,
+                   datetime.weekday};
+  }
 
   RTC8564.setDateTime(&newDatetime);
 }
@@ -72,11 +68,6 @@ void loop() {
 
   if (RTC8564.getDateTime(&dt) == 0) {
     sprintf(RTCTime, "%2d%2d", dt.hour, dt.minute);
-  }
-
-  if (RTC8564.getAlarmFlag()) {
-    mp3_play(4);
-    RTC8564.clearAlarmFlag();
   }
 
   String currentClock = RTCTime;
